63891c23191e13d86005c4eaf8554c7d6be99b22
sinelnikov7 <sinelnikovby@gmail.com>
2022-09-13 12:27:56 +0300

    5

--- writers/writers/settings.py
+++ writers/writers/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'writers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
