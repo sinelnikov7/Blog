52c8dacd4f54755f324f5c7da27f27733e896132
sinelnikov7 <sinelnikovby@gmail.com>
2022-09-20 20:10:50 +0300

    trying to do for pep8

--- writers/crud/forms.py
+++ writers/crud/forms.py
@@ -6,6 +6,5 @@ from api.models import Book
 class BookAddForm(forms.ModelForm):
 
     class Meta:
-
         model = Book
         fields = '__all__'
--- writers/crud/urls.py
+++ writers/crud/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 
 from .views import get_all_books, book_details, delete_book, ChangeBook
 
